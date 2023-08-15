17604a3013674537c75f8a435fa8cfa3ec9762b1
hayashady <85594498+hayashady@users.noreply.github.com>
2023-08-15 18:32:52 -0400

    Added models files

---
+++ microeconomic_models.py
@@ -0,0 +1,13 @@
+# Imperfect Competition
+    # Monopoloy
+    # Oligopoly
+    # Cournot Duopoloy
+    # Bertrand Duopoloy
+    # Stackelberg Duopoly
+    # Price Leadership
+    # Monopsony
+
+# Public Goods
+# Externalities
+
+# Intertemporal Optimization
