0168d39ab7d7f6226e83e0a0f63965fdc5702b90
hayashady <85594498+hayashady@users.noreply.github.com>
2023-09-04 20:09:54 -0400

    Added CFA IFP and FINRA SIE study materials

--- microeconomic_models.py
+++ microeconomic_models.py
@@ -1,13 +1,23 @@
+# Perfect Competition
+
 # Imperfect Competition
+
     # Monopoloy
+
     # Oligopoly
+
     # Cournot Duopoloy
+
     # Bertrand Duopoloy
+
     # Stackelberg Duopoly
+
     # Price Leadership
+    
     # Monopsony
 
 # Public Goods
+
 # Externalities
 
 # Intertemporal Optimization
